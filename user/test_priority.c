b5fb1e27c47a5897791d49c9d17c1ceaa6f1f2e1
Ruben Carmona <rcarmoname@miners.utep.edu>
2023-10-20 22:01:26 -0600

    Adding to user

---
+++ user/test_priority.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main() {
+    int i, pid;
+    int priorities[] = {30, 50, 20, 70, 10}; // Example priorities
+
+    for (i = 0; i < sizeof(priorities) / sizeof(priorities[0]); i++) {
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+            int priority = priorities[i];
+            printf("Setting priority for pid %d to %d\n", getpid(), priority);
+            setpriority(priority);
+            sleep(100); // Sleep for a while to allow other processes to run
+            exit(0);
+        } else if (pid > 0) {
+            // Parent process
+            wait(0);
+        } else {
+            // Fork failed
+            printf("Fork failed\n");
+            exit(1);
+        }
+    }
+
+    exit(0);
+}
