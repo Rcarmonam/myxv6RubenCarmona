78261abb230f354c5d8df86362b4e84097af9698
Ruben Carmona <rcarmoname@miners.utep.edu>
2023-10-20 22:29:17 -0600

    User

--- user/test_priority.c
+++ user/test_priority.c
@@ -1,6 +1,7 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/syscall.h"
 
 int main() {
     int i, pid;
--- user/user.h
+++ user/user.h
@@ -25,6 +25,7 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 int getprocs(struct pstat*);
+int setpriority(int priority);
 
 // ulib.c
 int stat(const char*, struct stat*);
